5203c95c3cba642948408ad687657521494ed867
noormohammad20 <mdsaim204@gmail.com>
2022-05-29 00:45:29 +0600

    SSR overview and dynamic route generation

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,14 +1,28 @@
 import React from 'react'
+import Link from 'next/link'
 
 const index = ({ users }) => {
     return (
         <div>
             <h2>This is users main page{users.length}</h2>
+            {
+                users.map(user => <div
+                    key={user.id}
+                >
+                    <h3>Name: {user.name}
+                        <li>
+                            <Link href={`/users/${user.id}`}>
+                                <button>Explore</button>
+                            </Link>
+                        </li></h3>
+                </div>)
+            }
         </div>
     )
 }
 
 export default index
+
 export async function getStaticProps(context) {
     const res = await fetch('https://jsonplaceholder.typicode.com/users')
     const data = await res.json()
---
+++ pages/users/managers.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const managers = () => {
+    return (
+        <div>
+            <h2>All Managers Are Hare</h2>
+        </div>
+    )
+}
+
+export default managers
